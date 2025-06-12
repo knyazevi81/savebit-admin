6f3116d215914c425ef90cdd25fb0d756e1fb730
Хасан Хамраев <khasanknyazev81@gmail.com>
2025-06-12 23:37:57 +0300

    nothing

--- src/database.py
+++ src/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
-
 engine = create_async_engine("sqlite+aiosqlite:///savebit_api.db")
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
